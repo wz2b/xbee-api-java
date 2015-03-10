2d51bc3f0c60d142f1d82eed8c645bd442331cdd
Christopher Piggott <chrisp@gmail.com>
2015-03-10 10:21:45 -0400

    Preparing for first release

--- src/main/java/com/autofrog/xbee/api/listeners/XbeeMessageListener.java
+++ src/main/java/com/autofrog/xbee/api/listeners/XbeeMessageListener.java
@@ -6,8 +6,8 @@ import com.autofrog.xbee.api.messages.XbeeMessageBase;
  *
  * @author chrisp
  */
-public interface XbeeMessageListener {
+public interface XbeeMessageListener<X extends XbeeMessageBase> {
 	
-		public void onXbeeMessage(Object sender, XbeeMessageBase msg);
+		public void onXbeeMessage(Object sender, X msg);
 
 }
