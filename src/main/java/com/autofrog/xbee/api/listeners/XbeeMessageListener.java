bd1b3aa067912e85947165eb59e38a9d4f5f0428
cepasp <cepasp@rit.edu>
2017-07-05 12:02:51 -0400

    Added separate component for marking when a piece of data was updated (as text)

--- src/main/java/com/autofrog/xbee/api/listeners/XbeeMessageListener.java
+++ src/main/java/com/autofrog/xbee/api/listeners/XbeeMessageListener.java
@@ -19,6 +19,6 @@ import com.autofrog.xbee.api.messages.XbeeMessageBase;
  */
 public interface XbeeMessageListener<X extends XbeeMessageBase> {
 	
-		public void onXbeeMessage(Object sender, X msg);
+		void onXbeeMessage(Object sender, X msg);
 
 }
